54710f21363eaacfecf6e29c89c1400ae4eb8be1
Chris King <cking5836@gmail.com>
2020-05-22 12:26:02 -0400

    Most done

--- challenges/closure.js
+++ challenges/closure.js
@@ -18,7 +18,16 @@ myFunction();
 
 // Explanation: 
 
+// Because the variable is blocked scoped, and because it is in the nested function, it can still be accessed.
 
 /* Task 2: Counter */
 
 /* Create a function called `sumation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+
+// function sumation(num) {
+//   return num * (num + 1) / 2;
+// }
+
+// console.log(sumation(4));
+
+//Done!!
